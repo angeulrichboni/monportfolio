f47d2ae9f87fa55c38ac49974cfb3e617d1aac90
Ange Ulrich BONI <boniangeulrich@gmail.com>
2025-10-22 10:57:06 +0100

    Updated with CV

--- components/CTAButtons.tsx
+++ components/CTAButtons.tsx
@@ -1,32 +1,33 @@
 "use client";
-import Link from "next/link";
 import { useI18n } from "./I18nProvider";
 
 export function CTAButtons() {
-  const { t } = useI18n();
+  const { t, lang } = useI18n();
+  const cvUrl =
+    lang === "fr"
+      ? "https://drive.google.com/file/d/1ixAWV4ogPEmDne-Af-qwZAJGpcdR3eSi/view?usp=sharing"
+      : "https://drive.google.com/file/d/16tiHoI8p2J-m_d6MTv8N6WNBmyRxu5UE/view?usp=sharing";
   return (
     <div className="flex flex-wrap gap-3">
-      <Link href="/cv.pdf" className="btn btn-primary" prefetch={false}>
+      <a href={cvUrl} className="btn btn-primary" target="_blank" rel="noopener noreferrer">
         {t("cta.downloadCv")}
-      </Link>
-      <Link
+      </a>
+      <a
         href="https://github.com/angeulrichboni"
         className="btn btn-outline"
         target="_blank"
-        rel="noreferrer"
-        prefetch={false}
+        rel="noopener noreferrer"
       >
         GitHub
-      </Link>
-      <Link
+      </a>
+      <a
         href="https://www.linkedin.com/in/acobe-ange-ulrich-boni/"
         className="btn btn-outline"
         target="_blank"
-        rel="noreferrer"
-        prefetch={false}
+        rel="noopener noreferrer"
       >
         LinkedIn
-      </Link>
+      </a>
     </div>
   );
 }
